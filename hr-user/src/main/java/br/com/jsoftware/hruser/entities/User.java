569fed049cf4ed073ee713eac4f838327c154fca
Jhonathan Martins <jhonathan.dsn.shop@alterdata.com.br>
2021-06-23 23:42:45 -0300

    Database seeding

--- hr-user/src/main/java/br/com/jsoftware/hruser/entities/User.java
+++ hr-user/src/main/java/br/com/jsoftware/hruser/entities/User.java
@@ -38,6 +38,6 @@ public class User implements Serializable {
 	private String password;
 
 	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
 }
