95cf5dd35af71021eb76bee40961179a94444c0d
Jhonathan Martins <jhonathan.dsn.shop@alterdata.com.br>
2021-07-28 02:28:55 -0300

    Security configs to config server

--- hr-oauth/src/main/java/br/com/jsoftware/hroauth/config/AuthorizationServerConfig.java
+++ hr-oauth/src/main/java/br/com/jsoftware/hroauth/config/AuthorizationServerConfig.java
@@ -1,6 +1,8 @@
 package br.com.jsoftware.hroauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,10 +14,18 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@RefreshScope
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
+
+	@Value("${oauth.client.name}")
+	private String clientName;
+	
+	@Value("${oauth.client.secret}")
+	private String clientSecret;
+	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 
@@ -36,8 +46,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory() // salva as configurações em memoria
-				.withClient("myappname123") // nome do app
-				.secret(passwordEncoder.encode("myappsecret123")) // Senha codificada do app
+				.withClient(clientName) // nome do app
+				.secret(passwordEncoder.encode(clientSecret)) // Senha codificada do app
 				.scopes("read", "write") // Define as permissões de leitura e escrita
 				.authorizedGrantTypes("password") // define o tipo do GrantType
 				.accessTokenValiditySeconds(86400); // define o tempo de expiração do token
@@ -48,5 +58,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore)
 				.accessTokenConverter(accessTokenConverter);
 	}
+	
 
 }
--- hr-oauth/src/main/java/br/com/jsoftware/hroauth/config/SecurityConfig.java
+++ hr-oauth/src/main/java/br/com/jsoftware/hroauth/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package br.com.jsoftware.hroauth.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,6 +19,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private UserDetailsService userDetailsService;
+	
+	private static final String PUBLIC = "/users/search**";
 
 	@Override
 	@Autowired
@@ -30,8 +34,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected AuthenticationManager authenticationManager() throws Exception {
 		return super.authenticationManager();
 	}
-	
-	
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, PUBLIC).permitAll();
+	}
 	
 
+	
 }
