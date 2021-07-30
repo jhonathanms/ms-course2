ff03d220e951ee85c7d5c156b7413745eab3b265
Jhonathan Martins <jhonathan.dsn.shop@alterdata.com.br>
2021-07-30 12:10:30 -0300

    db config server

--- hr_worker/src/main/java/com/jsoftware/hr_worker/resources/WorkerResource.java
+++ hr_worker/src/main/java/com/jsoftware/hr_worker/resources/WorkerResource.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,16 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jsoftware.hr_worker.entities.Worker;
 import com.jsoftware.hr_worker.repositories.WorkerRepository;
 
-@RefreshScope
 @RestController
 @RequestMapping(value= "/workers")
 public class WorkerResource {
 
 	@Autowired
 	private WorkerRepository repository;
-	
-	@Value("${test.config}")
-	private String configs;
 	
 	@Autowired
 	private Environment env;
@@ -41,7 +35,6 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs(){
-		logger.info("Configs:" + configs);
 		return ResponseEntity.noContent().build();
 	}
 	
